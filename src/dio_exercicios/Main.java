5320993e60e354c1176db93734a16a38e50b16ad
Perkles <jade.aabinajm@gmail.com>
2022-01-19 15:20:13 -0300

    add de codigo

---
+++ src/dio_exercicios/Main.java
@@ -0,0 +1,19 @@
+package dio_exercicios;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //quadrado
+        System.out.println("exercicio quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(8,7,9);
+        Quadrilatero.area(5f,5f);
+
+
+
+    }
+
+
+}
+
